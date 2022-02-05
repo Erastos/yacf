4e6d3ce19ea8c209ad096e302b104ec28abda2c8
Andrew Smith <andrew@andrewnsmith.net>
2022-02-04 21:39:41 -0500

    Comment code

--- process.cpp
+++ process.cpp
@@ -18,11 +18,16 @@ void Process::construct() {
     int pid;
     const char* array[_args.size()+2];
     array[0] = _name.c_str();
+    // First argument is the name of the application
+    // Iterate through to add the arguments to the array
     for (int i = 0; i < this->_args.size(); i++) {
         array[i+1] = this->_args[i].c_str();
     }
     array[_args.size()+1] = NULL;
     
+    // Add the required NULL to the end of the array
+    // Create an array to store the arguments
+    // Execute the provdied process application
     if ((pid = fork()) != 0) {
         int ret = execv(this->_name.c_str(), (char**)array);
         if (ret != 0) {
