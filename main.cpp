b4166fc92604fe95b2e0597a2e25b2203f1973a8
Andrew Smith <andrew@andrewnsmith.net>
2022-02-04 21:48:26 -0500

    Switch test application to sleep

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 
 int main (int argc, char* argv[]) {
     std::vector<std::string> args;
-    args.push_back("test");
-    std::string program = "/bin/echo";
+    args.push_back("10");
+    std::string program = "/bin/sleep";
     Process process(program, args);
+    // process.start();
     return 0;
 }
