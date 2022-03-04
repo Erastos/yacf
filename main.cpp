57031b2220c0abb04d4d87c7c146670ec06c2d6e
Andrew Smith <andrew@andrewnsmith.net>
2022-03-04 14:23:18 -0500

    Update test code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "process.h"
 #include <iostream>
-
+#include <string>
+#include <sys/wait.h>
+#include <memory>
 
 int main (int argc, char* argv[]) {
+    setvbuf(stdout, NULL, _IONBF, 0);
     std::vector<std::string> args;
-    args.push_back("10");
-    std::string program = "/bin/sleep";
-    Process process(program, args);
-    process.start();
+    // args.push_back("hello world");
+    std::string program = "/bin/sh";
+    std::unique_ptr<Process> process = std::make_unique<Process>(program, args);
+    process->start();
+    std::string a = "ls -la\n";
+    process->send(a);
+    usleep(4000);
+    std::string b = process->get(1000);
+    std::cout << "Contents: " << b << '\n';
+    std::string c = "cat test.txt\n";
+    process->send(c);
+    usleep(5000);
+    std::string d = process->get(1000);
+    std::cout << "Contents: " << d << '\n';
     return 0;
 }
