57843116a910129fa921ac3f1cef6655258f3a0a
Andrew Smith <andrew@andrewnsmith.net>
2022-10-28 09:33:09 -0600

    Make yacf a shared library

---
+++ test/main.cpp
@@ -0,0 +1,27 @@
+#include "../process.h"
+#include <iostream>
+#include <string>
+#include <sys/wait.h>
+#include <memory>
+
+int main (int argc, char* argv[]) {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    // args.push_back("hello world");
+    std::string program = "/bin/sh";
+    std::vector<std::string> args;
+    std::unique_ptr<Process> process = std::make_unique<Process>(program, args);
+    process->start();
+    // std::string a = process->get(100);
+    // std::cout << "Contents: " << a << "\n";
+    std::string a = "ls -la\n";
+    process->send(a);
+    // usleep(4000);
+    std::string b = process->get(1000);
+    std::cout << "Contents: " << b << '\n';
+    // std::string c = "cat test.txt\n";
+    // process->send(c);
+    // usleep(4000);
+    // std::string d = process->get(1000);
+    // std::cout << "Contents: " << d << '\n';
+    return 0;
+}
